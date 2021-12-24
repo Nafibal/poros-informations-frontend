d548998c321243a48a34944223708577c98530d9
Brian Aditya <brianadit33@gmail.com>
2021-12-24 21:13:03 +0800

    add navbar comp

---
+++ components/Navbar.js
@@ -0,0 +1,10 @@
+import styles from '../styles/Home.module.css'
+
+const Navbar = () => {
+  return (
+    <nav className={styles.porosBerita__nav}>
+      nav
+    </nav>
+  );
+}
+export default Navbar;
